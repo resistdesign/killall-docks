8df8480872b557c1da584ef5f972a9eb9619e4b1
Ryan Graff <ryantgraff@resistdesign.com>
2019-04-09 23:57:52 -0400

    feat: Add a basic, transparent window.

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
-const {
-  remote: {
-    BrowserWindow
-  } = {}
-} = require('electron');
+const {app, BrowserWindow} = require('electron');
 
-const MAIN_WINDOW = new BrowserWindow(
-  {
-    width: 800,
-    height: 600,
-    frame: false
+let MAIN_WINDOW;
+
+app.on(
+  'ready',
+  () => {
+    MAIN_WINDOW = new BrowserWindow(
+      {
+        width: 800,
+        height: 600,
+        center: true,
+        frame: false,
+        skipTaskbar: true,
+        alwaysOnTop: true,
+        useContentSize: true,
+        movable: false,
+        resizable: false,
+        minimizable: false,
+        maximizable: false,
+        closable: false,
+        focusable: false,
+        fullscreen: false,
+        fullscreenable: false,
+        transparent: true,
+        backgroundColor: 0x00000000,
+        hasShadow: false,
+        titleBarStyle: 'customButtonsOnHover',
+        show: false
+      }
+    );
+
+    MAIN_WINDOW.loadFile('./src/index.html');
+    MAIN_WINDOW.once(
+      'ready-to-show',
+      () => {
+        MAIN_WINDOW.show();
+      }
+    );
   }
 );
-
-MAIN_WINDOW.show();
